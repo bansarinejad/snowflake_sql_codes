a2c07060f6e23464a6c5aeb6ba75287deba7e420
behzad.ansarinejad <behzad.ansarinejad@gmail.com>
2024-07-27 18:50:59 +1000

    Initial commit

---
+++ W1_permanent_transient_tempprary_tables.sql
@@ -0,0 +1,40 @@
+CREATE TABLE tasty_bytes.raw_pos.truck_dev
+    CLONE tasty_bytes.raw_pos.truck;
+SELECT * FROM tasty_bytes.raw_pos.truck_dev;
+SET saved_query_id = LAST_QUERY_ID();
+SET saved_timestamp = CURRENT_TIMESTAMP;
+UPDATE tasty_bytes.raw_pos.truck_dev t
+    SET t.year = (YEAR(CURRENT_DATE()) -1000);
+
+---> drop truck_dev if not dropped previously
+DROP TABLE TASTY_BYTES.RAW_POS.TRUCK_DEV;
+
+---> create a transient table
+CREATE TRANSIENT TABLE TASTY_BYTES.RAW_POS.TRUCK_TRANSIENT
+    CLONE TASTY_BYTES.RAW_POS.TRUCK;
+
+---> create a temporary table
+CREATE TEMPORARY TABLE TASTY_BYTES.RAW_POS.TRUCK_TEMPORARY
+    CLONE TASTY_BYTES.RAW_POS.TRUCK;
+
+---> show tables that start with the word TRUCK
+SHOW TABLES LIKE 'TRUCK%';
+
+---> attempt (successfully) to set the data retention time to 90 days for the standard table
+ALTER TABLE TASTY_BYTES.RAW_POS.TRUCK SET DATA_RETENTION_TIME_IN_DAYS = 90;
+
+---> attempt (unsuccessfully) to set the data retention time to 90 days for the transient table
+ALTER TABLE TASTY_BYTES.RAW_POS.TRUCK_TRANSIENT SET DATA_RETENTION_TIME_IN_DAYS = 90;
+
+---> attempt (unsuccessfully) to set the data retention time to 90 days for the temporary table
+ALTER TABLE TASTY_BYTES.RAW_POS.TRUCK_TEMPORARY SET DATA_RETENTION_TIME_IN_DAYS = 90;
+
+SHOW TABLES LIKE 'TRUCK%';
+
+---> attempt (successfully) to set the data retention time to 0 days for the transient table
+ALTER TABLE TASTY_BYTES.RAW_POS.TRUCK_TRANSIENT SET DATA_RETENTION_TIME_IN_DAYS = 0;
+
+---> attempt (successfully) to set the data retention time to 0 days for the temporary table
+ALTER TABLE TASTY_BYTES.RAW_POS.TRUCK_TEMPORARY SET DATA_RETENTION_TIME_IN_DAYS = 0;
+
+SHOW TABLES LIKE 'TRUCK%';
